d11bb19a52774f1e5ab0ebbd5524a35ca464f4dc
mariaandrezacs <mandreza008@gmail.com>
2025-02-10 10:27:41 -0300

    feat: app settings

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+import time
+
+from flask_caching import Cache
+
+config = {
+    "CACHE_TYPE": "redis",
+    "CACHE_REDIS_HOST": "127.0.0.1",
+    "CACHE_REDIS_PORT": 6379,
+    "CACHE_REDIS_DB": 0,
+    "CACHE_REDIS_URL": "redis://127.0.0.1:6379/0",
+    "DEBUG": True,
+    "CACHE_DEFAULT_TIMEOUT": 300,
+}
+
+app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
+
+
+@app.route("/")
+@cache.cached(timeout=40, key_prefix="ola_mundo")
+def ola_mundo():
+    time.sleep(20)
+    cache.set("foo", "olá, salada")
+    return "oi"
+
+
+@app.route("/a")
+@cache.cached(timeout=40, key_prefix="alou")
+def alou():
+    time.sleep(20)
+    return "alou"
